5b36590fc6330481c338495ce2db5758d7bd29ec
Irvin Salazar <salazarirvin4@gmail.com>
2020-07-06 21:02:17 -0600

    initializing repository

---
+++ routes/posts.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const verify = require('./verifyToken');
+
+router.get('/', verify , (req,res) => {
+    /* res.json({
+        posts: {
+            title: 'My first post',
+            description: 'random data you shouldnt access'
+        }
+    }); */
+    res.send(req.user);
+    //User.findbyOne({_id: req.user})
+});
+
+
+
+
+
+
+
+module.exports = router;
